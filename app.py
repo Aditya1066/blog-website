4da8cc6e9c271498fe50f06f1d3d79203e7ff715
Aditya <aditya.agarwal1066@gmail.com>
2024-10-23 23:38:43 +0530

    updating 2 files

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+
+s = smtplib.SMTP('smtp.gmail.com', 587)
 
 app = Flask(__name__)
 
@@ -15,15 +18,28 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/contact')
+@app.route('/contact' , methods=['GET', 'POST'])
 def contact():
-    return render_template("contact.html")
+    if request.method == 'POST':
+        print(request.form['name'])
+        print(request.form['email'])
+        print(request.form['phone'])
+        print(request.form['message'])
+        s.starttls()
+        s.login("sender_email_id", "sender_email_id_password")
+        message = f"Name: {request.form['name']}, email: {request.form['email']}, phone: {request.form['phone']}, message: {request.form['message']}"
+        s.sendmail("sender_email_id", "receiver_email_id", message)
+        s.quit()
+        return render_template("contact.html", tex = f"Successfully sent your message.")
+    elif request.method == 'GET':
+        return render_template("contact.html", tex="Contact Me")
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     blog = all_blogs[post_id-1]
     img_name = f"assets/img/post-bg-{post_id}.jpg"
     return render_template("post.html", blog = blog, img_name=img_name)
+
 
 if __name__ == "__main__":
     app.run()
